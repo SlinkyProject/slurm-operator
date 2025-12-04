ea043e8e5dd9905656a5f8f9498281acd2322e55
Skyler Malinowski <skyler@schedmd.com>
2025-12-03 20:25:25 -0500

    feat: default accounting without cgroups to jobacct_gather/linux
    Changelog: Added - JobAcctGatherType will default to
     `jobacct_gather/linux` when accounting is enabled but cgroups is
     disabled.
    Cherry-picked: 42285d2d8e

--- internal/builder/controller_config.go
+++ internal/builder/controller_config.go
@@ -211,6 +211,8 @@ func buildSlurmConf(
 		conf.AddProperty(config.NewProperty("AccountingStorageTRES", "gres/gpu"))
 		if cgroupEnabled {
 			conf.AddProperty(config.NewProperty("JobAcctGatherType", "jobacct_gather/cgroup"))
+		} else {
+			conf.AddProperty(config.NewProperty("JobAcctGatherType", "jobacct_gather/linux"))
 		}
 	} else {
 		conf.AddProperty(config.NewProperty("AccountingStorageType", "accounting_storage/none"))
