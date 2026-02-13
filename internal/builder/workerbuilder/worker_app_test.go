4c1f0cbc9735aac1e4243e9b7b2d51ccfea0fef6
Vivian Hafener <vhafener@nvidia.com>
2026-02-13 14:48:42 -0700

    refactor: set slurmd pod resource limit env vars using operator
    Presently the environment variables used to communicate resource limits
    from Kubernetes to Slurm are set by the Helm charting. These should
    instead be set by the operator to provide more consistent application
    behavior for the creation and updating of NodeSets outside of the Helm
    Chart.
    
    Resolves: #255
    
    Changelog: changed - Move the setting of slurmd pod resource limit
     environment variables into slurm-operator from Helm chart

--- internal/builder/workerbuilder/worker_app_test.go
+++ internal/builder/workerbuilder/worker_app_test.go
@@ -12,6 +12,7 @@ import (
 	"github.com/SlinkyProject/slurm-operator/internal/builder/common"
 	"github.com/SlinkyProject/slurm-operator/internal/builder/labels"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	k8slabels "k8s.io/apimachinery/pkg/labels"
 	"k8s.io/utils/set"
@@ -102,6 +103,207 @@ func TestBuilder_BuildWorkerPodTemplate(t *testing.T) {
 
 			case len(got.Spec.DNSConfig.Searches) == 0:
 				t.Errorf("len(DNSConfig.Searches) = %v , want = > 0", len(got.Spec.DNSConfig.Searches))
+			}
+		})
+	}
+}
+
+func TestWorkerBuilder_getResourceLimits(t *testing.T) {
+	client := fake.NewFakeClient()
+
+	// Create values to test resource requirements with
+	cpu1, err := resource.ParseQuantity("1")
+	if err != nil {
+		t.Fatalf("Failed to call resource.ParseQuantity")
+	}
+
+	mem1g, err := resource.ParseQuantity("1Gi")
+	if err != nil {
+		t.Fatalf("Failed to call resource.ParseQuantity")
+	}
+
+	cpu4, err := resource.ParseQuantity("4")
+	if err != nil {
+		t.Fatalf("Failed to call resource.ParseQuantity")
+	}
+
+	mem2g, err := resource.ParseQuantity("2Gi")
+	if err != nil {
+		t.Fatalf("Failed to call resource.ParseQuantity")
+	}
+
+	type limits struct {
+		cpu    int64
+		memory int64
+	}
+	tests := []struct {
+		name    string
+		nodeset *slinkyv1beta1.NodeSetSpec
+		want    limits
+	}{
+		{
+			name:    "default - no limits",
+			nodeset: &slinkyv1beta1.NodeSetSpec{},
+			want: limits{
+				cpu:    0,
+				memory: 0,
+			},
+		},
+		{
+			name: "pod limits - cpu",
+			nodeset: &slinkyv1beta1.NodeSetSpec{
+				Template: slinkyv1beta1.PodTemplate{
+					PodSpecWrapper: slinkyv1beta1.PodSpecWrapper{
+						PodSpec: corev1.PodSpec{
+							Resources: &corev1.ResourceRequirements{
+								Limits: corev1.ResourceList{
+									"cpu": cpu1,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: limits{
+				cpu:    1,
+				memory: 0,
+			},
+		},
+		{
+			name: "pod limits - memory",
+			nodeset: &slinkyv1beta1.NodeSetSpec{
+				Template: slinkyv1beta1.PodTemplate{
+					PodSpecWrapper: slinkyv1beta1.PodSpecWrapper{
+						PodSpec: corev1.PodSpec{
+							Resources: &corev1.ResourceRequirements{
+								Limits: corev1.ResourceList{
+									"memory": mem1g,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: limits{
+				cpu:    0,
+				memory: 1024,
+			},
+		},
+		{
+			name: "pod limits - cpu & memory",
+			nodeset: &slinkyv1beta1.NodeSetSpec{
+				Template: slinkyv1beta1.PodTemplate{
+					PodSpecWrapper: slinkyv1beta1.PodSpecWrapper{
+						PodSpec: corev1.PodSpec{
+							Resources: &corev1.ResourceRequirements{
+								Limits: corev1.ResourceList{
+									"cpu":    cpu1,
+									"memory": mem1g,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: limits{
+				cpu:    1,
+				memory: 1024,
+			},
+		},
+		{
+			name: "container limits - cpu",
+			nodeset: &slinkyv1beta1.NodeSetSpec{
+				Slurmd: slinkyv1beta1.ContainerWrapper{
+					Container: corev1.Container{
+						Resources: corev1.ResourceRequirements{
+							Limits: corev1.ResourceList{
+								"cpu": cpu4,
+							},
+						},
+					},
+				},
+			},
+			want: limits{
+				cpu: 4,
+			},
+		},
+		{
+			name: "container limits - memory",
+			nodeset: &slinkyv1beta1.NodeSetSpec{
+				Slurmd: slinkyv1beta1.ContainerWrapper{
+					Container: corev1.Container{
+						Resources: corev1.ResourceRequirements{
+							Limits: corev1.ResourceList{
+								"memory": mem2g,
+							},
+						},
+					},
+				},
+			},
+			want: limits{
+				memory: 2048,
+			},
+		},
+		{
+			name: "container limits - cpu & memory",
+			nodeset: &slinkyv1beta1.NodeSetSpec{
+				Slurmd: slinkyv1beta1.ContainerWrapper{
+					Container: corev1.Container{
+						Resources: corev1.ResourceRequirements{
+							Limits: corev1.ResourceList{
+								"cpu":    cpu4,
+								"memory": mem2g,
+							},
+						},
+					},
+				},
+			},
+			want: limits{
+				cpu:    4,
+				memory: 2048,
+			},
+		},
+		{
+			name: "pod & container limits - cpu & memory",
+			nodeset: &slinkyv1beta1.NodeSetSpec{
+				Template: slinkyv1beta1.PodTemplate{
+					PodSpecWrapper: slinkyv1beta1.PodSpecWrapper{
+						PodSpec: corev1.PodSpec{
+							Resources: &corev1.ResourceRequirements{
+								Limits: corev1.ResourceList{
+									"cpu":    cpu4,
+									"memory": mem2g,
+								},
+							},
+						},
+					},
+				},
+				Slurmd: slinkyv1beta1.ContainerWrapper{
+					Container: corev1.Container{
+						Resources: corev1.ResourceRequirements{
+							Limits: corev1.ResourceList{
+								"cpu":    cpu1,
+								"memory": mem1g,
+							},
+						},
+					},
+				},
+			},
+			want: limits{
+				cpu:    1,
+				memory: 1024,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			b := New(client)
+			cpu, memory := b.getResourceLimits(tt.nodeset)
+			if cpu != tt.want.cpu {
+				t.Errorf("getResourceLimits() = %v, want %v", cpu, tt.want.cpu)
+			}
+			if memory != tt.want.memory {
+				t.Errorf("getResourceLimits() = %v, want %v", memory, tt.want.memory)
 			}
 		})
 	}
