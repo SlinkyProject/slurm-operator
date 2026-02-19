f058258a5103f5edc3e5438a6416b23b7b4d3abf
jamesbeedy <jamesbeedy@gmail.com>
2026-02-19 02:01:10 +0000

    fix: handle 0 in nodeset.slinky.slurm.net
    The NodeSet spec.replicas field is a pointer to int32 which uses the
    default minimum validation of 1. This minimum prevents KEDA from scaling
    nodesets to 0 replicas when no jobs are pending.
    
    Changes:
    - Update spec.replicas minimum from 1 to 0 in NodeSet CRD
    - Add mutating webhook to default nil replicas to 0
    - Update NodeSet CR template to use pointer for replicas field
    - Regenerate webhook manifests and CRDs
    
    This enables KEDA integration to scale nodesets down to 0 when idle,
    improving resource efficiency.

--- internal/webhook/nodeset_webhook_test.go
+++ internal/webhook/nodeset_webhook_test.go
@@ -4,7 +4,14 @@
 package webhook
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+
+	slinkyv1beta1 "github.com/SlinkyProject/slurm-operator/api/v1beta1"
 )
 
 var _ = Describe("NodeSet Webhook", func() {
@@ -15,6 +22,65 @@ var _ = Describe("NodeSet Webhook", func() {
 
 		It("Should admit if all required fields are provided", func() {
 			// TODO(user): Add your logic here
+		})
+	})
+
+	Context("When creating NodeSet under Mutating Webhook", func() {
+		var webhook *NodeSetWebhook
+
+		BeforeEach(func() {
+			webhook = &NodeSetWebhook{}
+		})
+
+		It("Should default nil replicas to 0", func() {
+			nodeset := &slinkyv1beta1.NodeSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-nodeset",
+					Namespace: "default",
+				},
+				Spec: slinkyv1beta1.NodeSetSpec{
+					Replicas: nil, // nil replicas
+				},
+			}
+
+			err := webhook.Default(context.Background(), nodeset)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(nodeset.Spec.Replicas).NotTo(BeNil())
+			Expect(*nodeset.Spec.Replicas).To(Equal(int32(0)))
+		})
+
+		It("Should not modify replicas when already set to 0", func() {
+			nodeset := &slinkyv1beta1.NodeSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-nodeset",
+					Namespace: "default",
+				},
+				Spec: slinkyv1beta1.NodeSetSpec{
+					Replicas: ptr.To[int32](0),
+				},
+			}
+
+			err := webhook.Default(context.Background(), nodeset)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(nodeset.Spec.Replicas).NotTo(BeNil())
+			Expect(*nodeset.Spec.Replicas).To(Equal(int32(0)))
+		})
+
+		It("Should not modify replicas when already set to a positive value", func() {
+			nodeset := &slinkyv1beta1.NodeSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-nodeset",
+					Namespace: "default",
+				},
+				Spec: slinkyv1beta1.NodeSetSpec{
+					Replicas: ptr.To[int32](5),
+				},
+			}
+
+			err := webhook.Default(context.Background(), nodeset)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(nodeset.Spec.Replicas).NotTo(BeNil())
+			Expect(*nodeset.Spec.Replicas).To(Equal(int32(5)))
 		})
 	})
 })
