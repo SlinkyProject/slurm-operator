c79a02ecfb43073c87948f43345ed83c58e91af4
Skyler Malinowski <smalinowski@nvidia.com>
2026-02-27 14:41:07 -0500

    fix(nodeset): fix incorrect object key being used in duration store
    Changelog: Fixed - Fixed case where a NodeSet pod attempting to be deleted
     but not drained yet would not correctly have its reconcile requeued to
     try again after it was marked DRAIN in Slurm.

--- internal/controller/nodeset/nodeset_sync.go
+++ internal/controller/nodeset/nodeset_sync.go
@@ -756,7 +756,6 @@ func (r *NodeSetReconciler) processCondemned(
 ) error {
 	logger := klog.FromContext(ctx)
 	pod := condemned[i]
-	key := objectutils.KeyFunc(pod)
 
 	podKey := client.ObjectKeyFromObject(pod)
 	if err := r.Get(ctx, podKey, pod); err != nil {
@@ -784,8 +783,9 @@ func (r *NodeSetReconciler) processCondemned(
 		}
 		// Decrement expectations and requeue reconcile because the Slurm node is not drained yet.
 		// We must wait until fully drained to terminate the pod.
-		durationStore.Push(key, 30*time.Second)
-		r.expectations.DeletionObserved(logger, objectutils.KeyFunc(nodeset), kubecontroller.PodKey(pod))
+		nodesetKey := objectutils.KeyFunc(nodeset)
+		durationStore.Push(nodesetKey, 30*time.Second)
+		r.expectations.DeletionObserved(logger, nodesetKey, kubecontroller.PodKey(pod))
 		return nil
 	}
 
