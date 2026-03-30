SlinkyProject/slurm-operator

commit cfe7c7cd89b1291f954702f3f7767875789106a6

refactor(crds): only return use pointers if required
The CRD helper functions were always returning non-nil pointers. This
changes the behavior to only only return a nil if required.

internal/utils/refresolver/refresolver.go
@@ -107,7 +107,7 @@ func (r *RefResolver) GetControllersForAccounting(ctx context.Context, accountin
 	return out, nil
 }
 
-func (r *RefResolver) GetSecretKeyRef(ctx context.Context, selector *corev1.SecretKeySelector, namespace string) ([]byte, error) {
+func (r *RefResolver) GetSecretKeyRef(ctx context.Context, selector corev1.SecretKeySelector, namespace string) ([]byte, error) {
 	secret := &corev1.Secret{}
 	key := types.NamespacedName{
 		Name:      selector.Name,

api/v1beta1/accounting_keys.go
@@ -50,9 +50,9 @@ func (o *Accounting) AuthStorageKey() types.NamespacedName {
 	}
 }
 
-func (o *Accounting) AuthStorageRef() *corev1.SecretKeySelector {
+func (o *Accounting) AuthStorageRef() corev1.SecretKeySelector {
 	authKey := o.AuthStorageKey()
-	return &corev1.SecretKeySelector{
+	return corev1.SecretKeySelector{
 		LocalObjectReference: corev1.LocalObjectReference{
 			Name: authKey.Name,
 		},
@@ -67,8 +67,8 @@ func (o *Accounting) AuthSlurmKey() types.NamespacedName {
 	}
 }
 
-func (o *Accounting) AuthSlurmRef() *corev1.SecretKeySelector {
-	return &o.Spec.SlurmKeyRef
+func (o *Accounting) AuthSlurmRef() corev1.SecretKeySelector {
+	return o.Spec.SlurmKeyRef
 }
 
 // Deprecated: use AuthJwtKey() instead.
@@ -77,33 +77,32 @@ func (o *Accounting) AuthJwtHs256Key() types.NamespacedName {
 }
 
 // Deprecated: use AuthJwtRef() instead.
-func (o *Accounting) AuthJwtHs256Ref() *corev1.SecretKeySelector {
+func (o *Accounting) AuthJwtHs256Ref() corev1.SecretKeySelector {
 	return o.AuthJwtRef()
 }
 
 func (o *Accounting) AuthJwtKey() types.NamespacedName {
-	refPtr := o.Spec.JwtHs256KeyRef
-	if o.Spec.JwtKeyRef != nil {
-		refPtr = o.Spec.JwtKeyRef
-	}
-	ref := ptr.Deref(refPtr, corev1.SecretKeySelector{})
+	ref := o.AuthJwtRef()
 	return types.NamespacedName{
 		Name:      ref.Name,
 		Namespace: o.Namespace,
 	}
 }
 
-func (o *Accounting) AuthJwtRef() *corev1.SecretKeySelector {
-	refPtr := o.Spec.JwtHs256KeyRef
-	if o.Spec.JwtKeyRef != nil {
+// NOTE: Return non-nil because this field is effectively required.
+func (o *Accounting) AuthJwtRef() corev1.SecretKeySelector {
+	var refPtr *corev1.SecretKeySelector
+	switch {
+	case o.Spec.JwtKeyRef != nil:
 		refPtr = o.Spec.JwtKeyRef
+	case o.Spec.JwtHs256KeyRef != nil:
+		refPtr = o.Spec.JwtHs256KeyRef
 	}
-	ref := ptr.Deref(refPtr, corev1.SecretKeySelector{})
-	return &ref
+	return ptr.Deref(refPtr, corev1.SecretKeySelector{})
 }
 
 func (o *Accounting) AuthJwksKey() types.NamespacedName {
-	ref := ptr.Deref(o.Spec.JwksKeyRef, corev1.ConfigMapKeySelector{})
+	ref := ptr.Deref(o.AuthJwksRef(), corev1.ConfigMapKeySelector{})
 	return types.NamespacedName{
 		Name:      ref.Name,
 		Namespace: o.Namespace,
@@ -111,8 +110,7 @@ func (o *Accounting) AuthJwksKey() types.NamespacedName {
 }
 
 func (o *Accounting) AuthJwksRef() *corev1.ConfigMapKeySelector {
-	ref := ptr.Deref(o.Spec.JwksKeyRef, corev1.ConfigMapKeySelector{})
-	return &ref
+	return o.Spec.JwksKeyRef
 }
 
 func (o *Accounting) ConfigKey() types.NamespacedName {

api/v1beta1/controller_keys.go
@@ -66,8 +66,8 @@ func (o *Controller) AuthSlurmKey() types.NamespacedName {
 	}
 }
 
-func (o *Controller) AuthSlurmRef() *corev1.SecretKeySelector {
-	return &o.Spec.SlurmKeyRef
+func (o *Controller) AuthSlurmRef() corev1.SecretKeySelector {
+	return o.Spec.SlurmKeyRef
 }
 
 // Deprecated: use AuthJwtKey() instead.
@@ -76,33 +76,32 @@ func (o *Controller) AuthJwtHs256Key() types.NamespacedName {
 }
 
 // Deprecated: use AuthJwtRef() instead.
-func (o *Controller) AuthJwtHs256Ref() *corev1.SecretKeySelector {
+func (o *Controller) AuthJwtHs256Ref() corev1.SecretKeySelector {
 	return o.AuthJwtRef()
 }
 
 func (o *Controller) AuthJwtKey() types.NamespacedName {
-	refPtr := o.Spec.JwtHs256KeyRef
-	if o.Spec.JwtKeyRef != nil {
-		refPtr = o.Spec.JwtKeyRef
-	}
-	ref := ptr.Deref(refPtr, corev1.SecretKeySelector{})
+	ref := o.AuthJwtRef()
 	return types.NamespacedName{
 		Name:      ref.Name,
 		Namespace: o.Namespace,
 	}
 }
 
-func (o *Controller) AuthJwtRef() *corev1.SecretKeySelector {
-	refPtr := o.Spec.JwtHs256KeyRef
-	if o.Spec.JwtKeyRef != nil {
+// NOTE: Return non-nil because this field is effectively required.
+func (o *Controller) AuthJwtRef() corev1.SecretKeySelector {
+	var refPtr *corev1.SecretKeySelector
+	switch {
+	case o.Spec.JwtKeyRef != nil:
 		refPtr = o.Spec.JwtKeyRef
+	case o.Spec.JwtHs256KeyRef != nil:
+		refPtr = o.Spec.JwtHs256KeyRef
 	}
-	ref := ptr.Deref(refPtr, corev1.SecretKeySelector{})
-	return &ref
+	return ptr.Deref(refPtr, corev1.SecretKeySelector{})
 }
 
 func (o *Controller) AuthJwksKey() types.NamespacedName {
-	ref := ptr.Deref(o.Spec.JwksKeyRef, corev1.ConfigMapKeySelector{})
+	ref := ptr.Deref(o.AuthJwksRef(), corev1.ConfigMapKeySelector{})
 	return types.NamespacedName{
 		Name:      ref.Name,
 		Namespace: o.Namespace,
@@ -110,8 +109,7 @@ func (o *Controller) AuthJwksKey() types.NamespacedName {
 }
 
 func (o *Controller) AuthJwksRef() *corev1.ConfigMapKeySelector {
-	ref := ptr.Deref(o.Spec.JwksKeyRef, corev1.ConfigMapKeySelector{})
-	return &ref
+	return o.Spec.JwksKeyRef
 }
 
 func (o *Controller) ConfigKey() types.NamespacedName {

api/v1beta1/controller_types.go
@@ -48,7 +48,7 @@ type ControllerSpec struct {
 
 	// accountingRef is a reference to the Accounting CR to which this has membership.
 	// +optional
-	AccountingRef ObjectReference `json:"accountingRef"`
+	AccountingRef *ObjectReference `json:"accountingRef,omitempty"`
 
 	// external indicates if this component is external to Kubernetes or not.
 	// If true, then externalConfig is used and other fields are ignored.

api/v1beta1/loginset_keys.go
@@ -44,9 +44,9 @@ func (o *LoginSet) SssdSecretKey() types.NamespacedName {
 	}
 }
 
-func (o *LoginSet) SssdSecretRef() *corev1.SecretKeySelector {
+func (o *LoginSet) SssdSecretRef() corev1.SecretKeySelector {
 	key := o.SssdSecretKey()
-	return &corev1.SecretKeySelector{
+	return corev1.SecretKeySelector{
 		LocalObjectReference: corev1.LocalObjectReference{
 			Name: key.Name,
 		},

api/v1beta1/nodeset_keys.go
@@ -32,9 +32,9 @@ func (o *NodeSet) SssdSecretKey() types.NamespacedName {
 	}
 }
 
-func (o *NodeSet) SssdSecretRef() *corev1.SecretKeySelector {
+func (o *NodeSet) SssdSecretRef() corev1.SecretKeySelector {
 	key := o.SssdSecretKey()
-	return &corev1.SecretKeySelector{
+	return corev1.SecretKeySelector{
 		LocalObjectReference: corev1.LocalObjectReference{
 			Name: key.Name,
 		},

api/v1beta1/token_keys.go
@@ -41,36 +41,32 @@ func (o *Token) JwtHs256Key() types.NamespacedName {
 }
 
 // Deprecated: use JwtRef() instead.
-func (o *Token) JwtHs256Ref() *JwtSecretKeySelector {
+func (o *Token) JwtHs256Ref() JwtSecretKeySelector {
 	return o.JwtRef()
 }
 
 func (o *Token) JwtKey() types.NamespacedName {
-	refPtr := o.Spec.JwtHs256KeyRef
-	if o.Spec.JwtKeyRef != nil {
-		refPtr = o.Spec.JwtKeyRef
-	}
-	ref := ptr.Deref(refPtr, JwtSecretKeySelector{})
-	namespace := ref.Namespace
-	if namespace == "" {
-		namespace = o.Namespace
-	}
+	ref := o.JwtRef()
 	return types.NamespacedName{
 		Name:      ref.Name,
-		Namespace: namespace,
+		Namespace: ref.Namespace,
 	}
 }
 
-func (o *Token) JwtRef() *JwtSecretKeySelector {
-	refPtr := o.Spec.JwtHs256KeyRef
-	if o.Spec.JwtKeyRef != nil {
+// NOTE: Return non-nil because this field is effectively required.
+func (o *Token) JwtRef() JwtSecretKeySelector {
+	var refPtr *JwtSecretKeySelector
+	switch {
+	case o.Spec.JwtKeyRef != nil:
 		refPtr = o.Spec.JwtKeyRef
+	case o.Spec.JwtHs256KeyRef != nil:
+		refPtr = o.Spec.JwtHs256KeyRef
 	}
 	ref := ptr.Deref(refPtr, JwtSecretKeySelector{})
 	if ref.Namespace == "" {
 		ref.Namespace = o.Namespace
 	}
-	return &ref
+	return ref
 }
 
 func (o *Token) SecretKey() types.NamespacedName {
@@ -84,13 +80,13 @@ func (o *Token) SecretKey() types.NamespacedName {
 	}
 }
 
-func (o *Token) SecretRef() *corev1.SecretKeySelector {
+func (o *Token) SecretRef() corev1.SecretKeySelector {
 	name := o.SecretKey().Name
 	key := "SLURM_JWT"
 	if o.Spec.SecretRef != nil {
 		key = o.Spec.SecretRef.Key
 	}
-	return &corev1.SecretKeySelector{
+	return corev1.SecretKeySelector{
 		LocalObjectReference: corev1.LocalObjectReference{
 			Name: name,
 		},

api/v1beta1/zz_generated.deepcopy.go
@@ -236,7 +236,11 @@ func (in *ControllerSpec) DeepCopyInto(out *ControllerSpec) {
 		*out = new(v1.ConfigMapKeySelector)
 		(*in).DeepCopyInto(*out)
 	}
-	out.AccountingRef = in.AccountingRef
+	if in.AccountingRef != nil {
+		in, out := &in.AccountingRef, &out.AccountingRef
+		*out = new(ObjectReference)
+		**out = **in
+	}
 	out.ExternalConfig = in.ExternalConfig
 	in.Slurmctld.DeepCopyInto(&out.Slurmctld)
 	in.Reconfigure.DeepCopyInto(&out.Reconfigure)

commit 525bb47f1ede8e0d500f29b4435fd8e96ebbfa05

fix: avoid nil accounting ptr deref
Regression introduced by commit cfe7c7cd.

Changelog: Fixed - Fixed operator panic when toggling accounting on or
 off.

internal/utils/refresolver/refresolver.go
@@ -92,6 +92,10 @@ func (r *RefResolver) GetRestapisForController(ctx context.Context, controller *
 }
 
 func (r *RefResolver) GetControllersForAccounting(ctx context.Context, accounting *slinkyv1beta1.Accounting) (*slinkyv1beta1.ControllerList, error) {
+	if accounting == nil {
+		return &slinkyv1beta1.ControllerList{}, nil
+	}
+
 	list := &slinkyv1beta1.ControllerList{}
 	if err := r.reader.List(ctx, list); err != nil {
 		return nil, err
@@ -99,7 +103,7 @@ func (r *RefResolver) GetControllersForAccounting(ctx context.Context, accountin
 
 	out := &slinkyv1beta1.ControllerList{}
 	for _, item := range list.Items {
-		if item.Spec.AccountingRef.IsMatch(objectutils.NamespacedName(accounting)) {
+		if item.Spec.AccountingRef != nil && item.Spec.AccountingRef.IsMatch(objectutils.NamespacedName(accounting)) {
 			out.Items = append(out.Items, item)
 		}
 	}