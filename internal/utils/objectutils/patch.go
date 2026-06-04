e23b3d49acc20003f4a608932670c82379059570
Skyler Malinowski <smalinowski@nvidia.com>
2026-06-04 14:40:29 -0400

    fix: use in memory object to create patch from
    Changelog: Fixed - Reduce object patch skew by using in memory object to
     generate patch from.

--- internal/utils/objectutils/patch.go
+++ internal/utils/objectutils/patch.go
@@ -272,7 +272,7 @@ func SyncObject(c client.Client, ctx context.Context, newObj client.Object, shou
 
 func PatchObject[T client.Object](c client.Client, ctx context.Context, obj T, mutateFn func(T) error) error {
 	key := client.ObjectKeyFromObject(obj)
-	patch, err := createPatch(c, ctx, obj, mutateFn)
+	patch, err := createPatch(obj, mutateFn)
 	if err != nil {
 		return fmt.Errorf("failed to create object patch for %s: %w", key, err)
 	}
@@ -290,7 +290,7 @@ func PatchObject[T client.Object](c client.Client, ctx context.Context, obj T, m
 
 func StatusPatchObject[T client.Object](c client.Client, ctx context.Context, obj T, mutateFn func(T) error) error {
 	key := client.ObjectKeyFromObject(obj)
-	patch, err := createPatch(c, ctx, obj, mutateFn)
+	patch, err := createPatch(obj, mutateFn)
 	if err != nil {
 		return fmt.Errorf("failed to create object patch for %s: %w", key, err)
 	}
@@ -306,13 +306,9 @@ func StatusPatchObject[T client.Object](c client.Client, ctx context.Context, ob
 	return nil
 }
 
-func createPatch[T client.Object](c client.Client, ctx context.Context, obj T, mutateFn func(T) error) (client.Patch, error) {
+func createPatch[T client.Object](obj T, mutateFn func(T) error) (client.Patch, error) {
 	if mutateFn == nil {
 		return nil, errors.New("mutateFn is required")
-	}
-	key := client.ObjectKeyFromObject(obj)
-	if err := c.Get(ctx, key, obj); err != nil {
-		return nil, fmt.Errorf("error getting %s: %w", key, err)
 	}
 	baseline, ok := obj.DeepCopyObject().(client.Object)
 	if !ok {
