fcf1de61b0a9f274f3ad94bad9b99869a4990376
Vivian Hafener <vhafener@nvidia.com>
2026-07-10 15:00:41 -0600

    chore: add logging for sync steps

--- internal/syncsteps/syncsteps.go
+++ internal/syncsteps/syncsteps.go
@@ -10,7 +10,9 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	utilerrors "k8s.io/apimachinery/pkg/util/errors"
 	"k8s.io/client-go/tools/events"
+	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 const syncAction = "Sync"
@@ -28,8 +30,11 @@ func Sync[T client.Object](
 	obj T,
 	steps []Step[T],
 ) error {
+	mainLogger := log.FromContext(ctx)
 	var errs []error
 	for _, s := range steps {
+		logger := mainLogger.WithValues("step", s.Name, "object", klog.KObj(obj))
+		logger.V(2).Info("Starting sync step")
 		if err := s.SyncFn(ctx, obj); err != nil {
 			msg := fmt.Sprintf("Failed %q step: %v", s.Name, err)
 			if recorder != nil {
@@ -37,9 +42,13 @@ func Sync[T client.Object](
 			}
 			errs = append(errs, fmt.Errorf("failed %q step: %w", s.Name, err))
 			if s.StopOnError {
+				logger.Error(err, "Failed sync step. Stopping sync for object...")
 				break
+			} else {
+				logger.Error(err, "Failed sync step. Continuing sync for object...")
 			}
 		}
+		logger.V(2).Info("Finished sync step")
 	}
 	return utilerrors.NewAggregate(errs)
 }
