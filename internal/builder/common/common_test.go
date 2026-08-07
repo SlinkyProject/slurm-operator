18e324fe29fe1edf311aefc68f0480e9f10bb888
Skyler Malinowski <smalinowski@nvidia.com>
2026-08-07 15:08:56 -0400

    test: fix require.Equal arg order
    Changelog: NONE

--- internal/builder/common/common_test.go
+++ internal/builder/common/common_test.go
@@ -63,7 +63,7 @@ func TestConfiglessArgs(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := ConfiglessArgs(tt.controller)
-			require.Equal(t, got, tt.want)
+			require.Equal(t, tt.want, got)
 		})
 	}
 }
