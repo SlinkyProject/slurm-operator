b5930fa056807a0ea55f67bafafcfc9689eee272
Skyler Malinowski <smalinowski@nvidia.com>
2026-09-02 12:26:00 -0400

    test: add configHash delta detection

--- internal/builder/controllerbuilder/controller_app_test.go
+++ internal/builder/controllerbuilder/controller_app_test.go
@@ -7,15 +7,18 @@ import (
 	_ "embed"
 	"testing"
 
-	slinkyv1beta1 "github.com/SlinkyProject/slurm-operator/api/v1beta1"
-	"github.com/SlinkyProject/slurm-operator/internal/builder/common"
-	"github.com/SlinkyProject/slurm-operator/internal/builder/labels"
+	"github.com/stretchr/testify/require"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/utils/ptr"
 	"k8s.io/utils/set"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	slinkyv1beta1 "github.com/SlinkyProject/slurm-operator/api/v1beta1"
+	"github.com/SlinkyProject/slurm-operator/internal/builder/common"
+	"github.com/SlinkyProject/slurm-operator/internal/builder/labels"
+	"github.com/SlinkyProject/slurm-operator/internal/utils/crypto"
 )
 
 func TestBuilder_BuildController(t *testing.T) {
@@ -206,6 +209,108 @@ func BenchmarkBuilder_BuildController(b *testing.B) {
 			for b.Loop() {
 				build.BuildController(bb.args.controller) //nolint:errcheck
 			}
+		})
+	}
+}
+
+func TestBuilder_BuildController_configHash(t *testing.T) {
+	primary := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{Name: "slurm-config", Namespace: "ns"},
+		Data:       map[string]string{"slurm.conf": "ClusterName=foo"},
+	}
+	extra := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{Name: "extra-conf", Namespace: "ns"},
+		Data:       map[string]string{"cgroup.conf": "ConstrainCores=yes"},
+	}
+	extraChanged := extra.DeepCopy()
+	extraChanged.Data["cgroup.conf"] = "ConstrainCores=no"
+	controller := func(inplace bool, refs ...string) *slinkyv1beta1.Controller {
+		c := &slinkyv1beta1.Controller{
+			ObjectMeta: metav1.ObjectMeta{Name: "slurm", Namespace: "ns"},
+			Spec: slinkyv1beta1.ControllerSpec{
+				JwtKeyRef:          &corev1.SecretKeySelector{},
+				InplaceReconfigure: inplace,
+			},
+		}
+		for _, name := range refs {
+			c.Spec.ConfigFileRefs = append(c.Spec.ConfigFileRefs, corev1.LocalObjectReference{Name: name})
+		}
+		return c
+	}
+
+	type fields struct {
+		client client.Client
+	}
+	type args struct {
+		controller *slinkyv1beta1.Controller
+	}
+	tests := []struct {
+		name     string
+		fields   fields
+		args     args
+		wantHash string
+	}{
+		{
+			name: "inplace skips config hash",
+			fields: fields{
+				client: fake.NewFakeClient(primary, extra),
+			},
+			args: args{
+				controller: controller(true, "extra-conf"),
+			},
+		},
+		{
+			name: "primary config only",
+			fields: fields{
+				client: fake.NewFakeClient(primary),
+			},
+			args: args{
+				controller: controller(false),
+			},
+			wantHash: crypto.CheckSumFromMap(primary.Data),
+		},
+		{
+			name: "includes ConfigFileRefs",
+			fields: fields{
+				client: fake.NewFakeClient(primary, extra),
+			},
+			args: args{
+				controller: controller(false, "extra-conf"),
+			},
+			wantHash: crypto.CheckSumFromMap(map[string]string{
+				"slurm.conf":  "ClusterName=foo",
+				"cgroup.conf": "ConstrainCores=yes",
+			}),
+		},
+		{
+			name: "ConfigFileRefs change hash",
+			fields: fields{
+				client: fake.NewFakeClient(primary, extraChanged),
+			},
+			args: args{
+				controller: controller(false, "extra-conf"),
+			},
+			wantHash: crypto.CheckSumFromMap(map[string]string{
+				"slurm.conf":  "ClusterName=foo",
+				"cgroup.conf": "ConstrainCores=no",
+			}),
+		},
+		{
+			name: "missing ConfigFileRefs ConfigMap",
+			fields: fields{
+				client: fake.NewFakeClient(primary),
+			},
+			args: args{
+				controller: controller(false, "extra-conf"),
+			},
+			wantHash: crypto.CheckSumFromMap(primary.Data),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			sts, err := New(tt.fields.client).BuildController(tt.args.controller)
+			require.NoError(t, err)
+			require.Equal(t, tt.wantHash, sts.Spec.Template.Annotations[annotationSlurmConfigHash])
 		})
 	}
 }
